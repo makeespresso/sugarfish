945fe0cfdeada79f1f5ebe528624870cf715f112
Makeespresso <makeespresso@gmail.com>
2019-07-29 14:35:48 -0400

    navbar set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import NavBar from './components/NavBar';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div className="navbar">
+      <ul>
+        <li><a href="#">Story</a></li>
+        <li><a href="#">Menu</a></li>
+        <li><a href="#">Image</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="https://postmates.com/merchant/sugarfish-new-york-2" target="_blank">Order</a></li>
+      </ul>
+    </div>
+  )
+}
