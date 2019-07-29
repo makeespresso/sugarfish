e661bae9a157920c528ea25a01b91dfce540b99e
Makeespresso <makeespresso@gmail.com>
2019-07-29 15:47:12 -0400

    menu set up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import NavBar from './components/NavBar';
-
 import './App.css';
+import NavBar from './components/NavBar';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Menu />
     </div>
   );
 }
