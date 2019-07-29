4cd04fafb5035321f2a295e811c7fd87b795b639
Makeespresso <makeespresso@gmail.com>
2019-07-29 19:16:23 -0400

    navbar-update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,10 +6,10 @@ export default () => {
       <ul>
         <li><a href="#">Story</a></li>
         <li><a href="#">Menu</a></li>
-        <li><a href="#"><img src="/img/sugar-logo.png" alt="" /></a></li>
+        <li><a href="#"><img src="/img/sugar-logo.png" alt="Sugarfish-logo" /></a></li>
         <li><a href="#">Contact</a></li>
         <li><a href="https://postmates.com/merchant/sugarfish-new-york-2" target="_blank">Order</a></li>
       </ul>
-    </div>
+    </div >
   )
 }
