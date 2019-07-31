7e1c9a5e10edfd460a5a64a8e7f57db4012dd3bd
Alexander Curtin <alexcurtin@Alexanders-MBP.fios-router.home>
2019-07-30 21:02:59 -0400

    style navbar a little

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ export default () => {
       <ul>
         <li><a href="#">Story</a></li>
         <li><a href="#">Menu</a></li>
-        <li><a href="#"><img src="/img/sugar-logo.png" alt="Sugarfish-logo" /></a></li>
+        <li className="fish-logo"><a href="#"><img src="/img/sugar-logo.png" alt="Sugarfish-logo" /></a></li>
         <li><a href="#">Contact</a></li>
         <li><a href="https://postmates.com/merchant/sugarfish-new-york-2" target="_blank">Order</a></li>
       </ul>
