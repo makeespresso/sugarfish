5291e8d2657b97b3d87892805c92ebdb61b2f08d
Makeespresso <makeespresso@gmail.com>
2019-08-05 09:10:12 -0400

    menu banner

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,118 +2,125 @@ import React from 'react'
 
 export default function Menu() {
   return (
-    <div id="menu" className="menu-border">
-      <div className="special-container">
-        <div className="special-div">
-          <h3>TURST ME</h3>
-          <p>Organic Edamame</p>
-          <p>Tuna Sashimi</p>
-          <p>Albacore Sushi (2-pc)</p>
-          <p>Salmon Sushi (2-pc)</p>
-          <p>Toro Hand Roll</p>
-          <p>Yellowtail* Sushi (2-pc)</p>
-          <p>Sea Bass Sushi (1-pc)</p>
-          <p>NZ Sea Bream* Sushi (1-pc)</p>
-          <p>Crab Hand Roll</p>
-          <h4>$40</h4>
-        </div>
-        <div className="special-div">
-          <h3>TURST ME LITE</h3>
-          <p>Tuna Sashimi</p>
-          <p>Albacore Sushi (2-pc)</p>
-          <p>Organic Edamame</p>
-          <p>Salmon Sushi (2-pc) </p>
-          <p>Toro Hand Roll </p>
-          <p>Yellowtail* Sushi (2-pc)</p>
-          <p>Sea Bass Sushi (1-pc)</p>
-          <p>NZ Sea Bream* Sushi (1-pc)</p>
-          <p>Crab Hand Roll</p>
-          <h4>$28</h4>
-        </div>
-        <div className="special-div">
-          <h3>THE NOZAWA</h3>
-          <p>Organic Edamame</p>
-          <p>Tuna Sashimi</p>
-          <p>Albacore Sushi (2-pc)</p>
-          <p>Salmon Sushi (2-pc) </p>
-          <p>Toro Hand Roll </p>
-          <p>Yellowtail* Sushi (2-pc)</p>
-          <p>Sea Bass Sushi (1-pc)</p>
-          <p>NZ Sea Bream* Sushi (1-pc)</p>
-          <p>Crab Hand Roll</p>
-          <h4>$52</h4>
-        </div>
-
+    <>
+      <div className="banner story-banner">
+        <h2 className="banner-title">MENU</h2>
+        <img className="banner-img" src="/img/desktop_view_menu_banner.png" alt="a bunch of fish" />
       </div>
-      <div className="special-container">
-        <div className="bottom-div">
-          <div className="inner-menu">
-            <p className="extra-space"><b>Sashimi + Edamame</b></p>
-            <p>Organic Edamame <span>$3.50</span></p>
-            <p>Salmon Sashimi <span>$9.25</span></p>
-            <p>Tuna Sashimi <span>$10.00</span></p>
-            <br />
-            <p><b>Sushi ( 2 pieces per order )</b></p>
-            <p>Bay Scallops <span>$6.00</span></p>
-            <p>Nozawa-style Shrimp <span>$6.50</span></p>
-            <p>Tuna <span>$6.75</span></p>
-            <p>Unagi <span>$6.75</span></p>
-            <p>Albacore<span>$7.00</span></p>
-            <p>Large Scallops <span>$7.00</span></p>
-            <p>Salmon <span>$7.00</span></p>
-            <p>NZ Sea Bream* <span>$7.25</span></p>
-            <p>Sea Bass <span>$7.25</span></p>
-            <p>Salmon Kombu <span>$7.50</span></p>
-            <p>Yellowtail* <span>$7.50</span></p>
-          </div>
-        </div >
-        <div className="bottom-div">
-          <div className="just-eat-container">
-            <h4><b>Dont’t Think. Just Eat.</b></h4>
-            <p>$63.00</p>
-          </div>
-          <div className="just-eat-bottom">
-            <div className="just-eat-left-div">
-              <p className="extra-space"><b>Hand Rolls & Cut Rolls</b></p>
-              <p>Cucumber</p>
-              <p>Bay Scallops</p>
-              <p>Salmon</p>
-              <p>Crab</p>
-              <p>Toro “Fatty Tuna”</p>
-              <p>Yellowtail*</p>
-              <p>Lobster</p>
-              <p>Split Cut Roll</p>
+      <div id="menu">
+        <div className="menu-border">
+          <div className="special-container">
+            <div className="special-div">
+              <h3>TURST ME</h3>
+              <p>Organic Edamame</p>
+              <p>Tuna Sashimi</p>
+              <p>Albacore Sushi (2-pc)</p>
+              <p>Salmon Sushi (2-pc)</p>
+              <p>Toro Hand Roll</p>
+              <p>Yellowtail* Sushi (2-pc)</p>
+              <p>Sea Bass Sushi (1-pc)</p>
+              <p>NZ Sea Bream* Sushi (1-pc)</p>
+              <p>Crab Hand Roll</p>
+              <h4>$40</h4>
             </div>
-            <div className="just-eat-right-div">
-              <p className="extra-space"><small><b>Hand</b></small> <span><small><b>Cut 8-pc</b></small></span></p>
-              <p>$5.25 <span>$9.50</span></p>
-              <p>$5.75 <span>$11.25</span></p>
-              <p>$5.75 <span>$11.25</span></p>
-              <p>$6.00 <span>$11.50</span></p>
-              <p>$6.50 <span>$12.25</span></p>
-              <p>$6.50 <span>$12.25</span></p>
-              <p>$9.50 <span>$18.25</span></p>
-              <p>$16.50</p>
+            <div className="special-div">
+              <h3>TURST ME LITE</h3>
+              <p>Tuna Sashimi</p>
+              <p>Albacore Sushi (2-pc)</p>
+              <p>Organic Edamame</p>
+              <p>Salmon Sushi (2-pc) </p>
+              <p>Toro Hand Roll </p>
+              <p>Yellowtail* Sushi (2-pc)</p>
+              <p>Sea Bass Sushi (1-pc)</p>
+              <p>NZ Sea Bream* Sushi (1-pc)</p>
+              <p>Crab Hand Roll</p>
+              <h4>$28</h4>
             </div>
-          </div>
-        </div>
-        <div className="bottom-div">
-          <h6>LIMITED AVAILABILITY ITEMS</h6>
-          <div className="limited-menu">
-            <p>Albacore Belly <span>$7.50</span></p>
-            <p>Kampachi<span>$8.00</span></p>
-            <p>NZ Sea Bream* with Shiso<span>$8.00</span></p>
-            <p>Oyster (Sashimi)<span>$8.00</span></p>
-            <p>Salmon Eggs<span>$8.25</span></p>
-            <p>Sweet Shrimp<span>$9.25</span></p>
-            <p>Ankimo (Sashimi)<span>$9.75</span></p>
-            <p>Anago <span>$10.00</span></p>
-            <p>Uni <span>$20.00</span></p>
-            <p>Toro <span>$10-$16</span></p>
-          </div>
-        </div>
+            <div className="special-div">
+              <h3>THE NOZAWA</h3>
+              <p>Organic Edamame</p>
+              <p>Tuna Sashimi</p>
+              <p>Albacore Sushi (2-pc)</p>
+              <p>Salmon Sushi (2-pc) </p>
+              <p>Toro Hand Roll </p>
+              <p>Yellowtail* Sushi (2-pc)</p>
+              <p>Sea Bass Sushi (1-pc)</p>
+              <p>NZ Sea Bream* Sushi (1-pc)</p>
+              <p>Crab Hand Roll</p>
+              <h4>$52</h4>
+            </div>
 
+          </div>
+          <div className="special-container">
+            <div className="bottom-div">
+              <div className="inner-menu">
+                <p className="extra-space"><b>Sashimi + Edamame</b></p>
+                <p>Organic Edamame <span>$3.50</span></p>
+                <p>Salmon Sashimi <span>$9.25</span></p>
+                <p>Tuna Sashimi <span>$10.00</span></p>
+                <br />
+                <p><b>Sushi ( 2 pieces per order )</b></p>
+                <p>Bay Scallops <span>$6.00</span></p>
+                <p>Nozawa-style Shrimp <span>$6.50</span></p>
+                <p>Tuna <span>$6.75</span></p>
+                <p>Unagi <span>$6.75</span></p>
+                <p>Albacore<span>$7.00</span></p>
+                <p>Large Scallops <span>$7.00</span></p>
+                <p>Salmon <span>$7.00</span></p>
+                <p>NZ Sea Bream* <span>$7.25</span></p>
+                <p>Sea Bass <span>$7.25</span></p>
+                <p>Salmon Kombu <span>$7.50</span></p>
+                <p>Yellowtail* <span>$7.50</span></p>
+              </div>
+            </div >
+            <div className="bottom-div">
+              <div className="just-eat-container">
+                <h4><b>Dont’t Think. Just Eat.</b></h4>
+                <p>$63.00</p>
+              </div>
+              <div className="just-eat-bottom">
+                <div className="just-eat-left-div">
+                  <p className="extra-space"><b>Hand Rolls & Cut Rolls</b></p>
+                  <p>Cucumber</p>
+                  <p>Bay Scallops</p>
+                  <p>Salmon</p>
+                  <p>Crab</p>
+                  <p>Toro “Fatty Tuna”</p>
+                  <p>Yellowtail*</p>
+                  <p>Lobster</p>
+                  <p>Split Cut Roll</p>
+                </div>
+                <div className="just-eat-right-div">
+                  <p className="extra-space"><small><b>Hand</b></small> <span><small><b>Cut 8-pc</b></small></span></p>
+                  <p>$5.25 <span>$9.50</span></p>
+                  <p>$5.75 <span>$11.25</span></p>
+                  <p>$5.75 <span>$11.25</span></p>
+                  <p>$6.00 <span>$11.50</span></p>
+                  <p>$6.50 <span>$12.25</span></p>
+                  <p>$6.50 <span>$12.25</span></p>
+                  <p>$9.50 <span>$18.25</span></p>
+                  <p>$16.50</p>
+                </div>
+              </div>
+            </div>
+            <div className="bottom-div">
+              <h6>LIMITED AVAILABILITY ITEMS</h6>
+              <div className="limited-menu">
+                <p>Albacore Belly <span>$7.50</span></p>
+                <p>Kampachi<span>$8.00</span></p>
+                <p>NZ Sea Bream* with Shiso<span>$8.00</span></p>
+                <p>Oyster (Sashimi)<span>$8.00</span></p>
+                <p>Salmon Eggs<span>$8.25</span></p>
+                <p>Sweet Shrimp<span>$9.25</span></p>
+                <p>Ankimo (Sashimi)<span>$9.75</span></p>
+                <p>Anago <span>$10.00</span></p>
+                <p>Uni <span>$20.00</span></p>
+                <p>Toro <span>$10-$16</span></p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
